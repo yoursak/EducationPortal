cf441f7ce1ac696565853723722b81e0cfc29f7b
Aman Sahu <amansahu2333924@gmail.com>
2023-08-12 12:26:09 +0530

    ED_10

---
+++ resources/views/tutorials/layout/script.blade.php
@@ -0,0 +1,12 @@
+<script src="{{asset('assetss/js/jquery-3.6.0.min.js')}}"></script>
+
+<script src="{{asset('assetss/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<script src="{{asset('assetss/js/feather.min.js')}}"></script>
+
+<script src="{{asset('assetss/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+<script src="{{asset('assetss/plugins/apexchart/apexcharts.min.js')}}"></script>
+<script src="{{asset('assetss/plugins/apexchart/chart-data.js')}}"></script>
+
+<script src="{{asset('assetss/js/script.js')}}"></script>
